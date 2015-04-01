40aa4792c5be1110200fca144eadc6d4dbbedfa4
xelmirage at xelmirage-pc <xelmirage@pku.edu.cn>
2015-04-01 09:09:00 +0800

    begin-dist-qrfac

--- pano13/lmdif.c
+++ pano13/lmdif.c
@@ -31,6 +31,7 @@ double DWARF = 1.0e-38;
 
 int fdjac2(int,int,double*,double*,double*,int,int*,double,double*);
 int qrfac(int,int,double*,int,int,int*,int,double*,double*,double*);
+int qrfac_dist(int, int, double*, int, int, int*, int, double*, double*, double*);
 int lmpar(int,double*,int,int*,double*,double*,double,double*,double*,double*,double*,double*);
 int qrsolv(int,double*,int,int*,double*,double*,double*,double*,double*);
 
@@ -897,10 +898,10 @@ L30:
 	/*
 	*	 compute the qr factorization of the jacobian.
 	*/
-#ifdef _DEBUG
-	TIMETRACE("qrfac",qrfac(m,n,fjac,ldfjac,1,ipvt,n,wa1,wa2,wa3);;) 
+#ifndef _DEBUG
+	TIMETRACE("qrfac",qrfac_dist(m,n,fjac,ldfjac,1,ipvt,n,wa1,wa2,wa3);;) 
 #else
-	qrfac(m,n,fjac,ldfjac,1,ipvt,n,wa1,wa2,wa3);
+	qrfac_dist(m,n,fjac,ldfjac,1,ipvt,n,wa1,wa2,wa3);
 #endif
 	
 	/*
@@ -1708,6 +1709,226 @@ L40:
 		}
 
 L100:
+		rdiag[j] = -ajnorm;
+	}
+	/*
+	*     last card of subroutine qrfac.
+	*/
+	return 0;
+}
+struct qrfac_para
+{
+	int m; int n;
+	double *a; int lda PT_UNUSED; int pivot;
+	int *ipvt; int lipvt PT_UNUSED; double *rdiag;
+	double *acnorm; double *wa;
+};
+int qrfac_thread(struct qrfac_para* para)
+{
+	return 0;
+}
+int qrfac_dist(int m, int n, double a[], int lda PT_UNUSED, int pivot,
+	int ipvt[], int lipvt PT_UNUSED, double rdiag[],
+	double acnorm[], double wa[])
+{
+	/*
+	*     **********
+	*
+	*     subroutine qrfac
+	*
+	*     this subroutine uses householder transformations with column
+	*     pivoting (optional) to compute a qr factorization of the
+	*     m by n matrix a. that is, qrfac determines an orthogonal
+	*     matrix q, a permutation matrix p, and an upper trapezoidal
+	*     matrix r with diagonal elements of nonincreasing magnitude,
+	*     such that a*p = q*r. the householder transformation for
+	*     column k, k = 1,2,...,min(m,n), is of the form
+	*
+	*			    t
+	*	    i - (1/u(k))*u*u
+	*
+	*     where u has zeros in the first k-1 positions. the form of
+	*     this transformation and the method of pivoting first
+	*     appeared in the corresponding linpack subroutine.
+	*
+	*     the subroutine statement is
+	*
+	*	subroutine qrfac(m,n,a,lda,pivot,ipvt,lipvt,rdiag,acnorm,wa)
+	*
+	*     where
+	*
+	*	m is a positive integer input variable set to the number
+	*	  of rows of a.
+	*
+	*	n is a positive integer input variable set to the number
+	*	  of columns of a.
+	*
+	*	a is an m by n array. on input a contains the matrix for
+	*	  which the qr factorization is to be computed. on output
+	*	  the strict upper trapezoidal part of a contains the strict
+	*	  upper trapezoidal part of r, and the lower trapezoidal
+	*	  part of a contains a factored form of q (the non-trivial
+	*	  elements of the u vectors described above).
+	*
+	*	lda is a positive integer input variable not less than m
+	*	  which specifies the leading dimension of the array a.
+	*
+	*	pivot is a logical input variable. if pivot is set true,
+	*	  then column pivoting is enforced. if pivot is set false,
+	*	  then no column pivoting is done.
+	*
+	*	ipvt is an integer output array of length lipvt. ipvt
+	*	  defines the permutation matrix p such that a*p = q*r.
+	*	  column j of p is column ipvt(j) of the identity matrix.
+	*	  if pivot is false, ipvt is not referenced.
+	*
+	*	lipvt is a positive integer input variable. if pivot is false,
+	*	  then lipvt may be as small as 1. if pivot is true, then
+	*	  lipvt must be at least n.
+	*
+	*	rdiag is an output array of length n which contains the
+	*	  diagonal elements of r.
+	*
+	*	acnorm is an output array of length n which contains the
+	*	  norms of the corresponding columns of the input matrix a.
+	*	  if this information is not needed, then acnorm can coincide
+	*	  with rdiag.
+	*
+	*	wa is a work array of length n. if pivot is false, then wa
+	*	  can coincide with rdiag.
+	*
+	*     subprograms called
+	*
+	*	minpack-supplied ... dpmpar,enorm
+	*
+	*	fortran-supplied ... dmax1,dsqrt
+	*
+	*     argonne national laboratory. minpack project. march 1980.
+	*     burton s. garbow, kenneth e. hillstrom, jorge j. more
+	*
+	*     **********
+	*/
+	int i, ij, jj, j, jp1, k, kmax, minmn;
+	double ajnorm, sum, temp;
+	static double zero = 0.0;
+	static double one = 1.0;
+	static double p05 = 0.05;
+	extern double MACHEP;
+	/*
+	*     compute the initial column norms and initialize several arrays.
+	*/
+	ij = 0;
+
+	for (j = 0; j < n; j++)
+	{
+		acnorm[j] = enorm(m, &a[ij]);
+		rdiag[j] = acnorm[j];
+		wa[j] = rdiag[j];
+		if (pivot != 0)
+			ipvt[j] = j;
+		ij += m; /* m*j */
+	}
+
+#if BUG
+	printf("qrfac\n");
+#endif
+	/*
+	*     reduce a to r with householder transformations.
+	*/
+	minmn = m <= n ? m : n;
+	for (j = 0; j<minmn; j++)
+	{
+		if (pivot == 0)
+			goto L40;
+		/*
+		*	 bring the column of largest norm into the pivot position.
+		*/
+		kmax = j;
+		for (k = j; k<n; k++)
+		{
+			if (rdiag[k] > rdiag[kmax])
+				kmax = k;
+		}
+		if (kmax == j)
+			goto L40;
+
+		ij = m * j;
+		jj = m * kmax;
+		for (i = 0; i<m; i++)
+		{
+			temp = a[ij]; /* [i+m*j] */
+			a[ij] = a[jj]; /* [i+m*kmax] */
+			a[jj] = temp;
+			ij += 1;
+			jj += 1;
+		}
+		rdiag[kmax] = rdiag[j];
+		wa[kmax] = wa[j];
+		k = ipvt[j];
+		ipvt[j] = ipvt[kmax];
+		ipvt[kmax] = k;
+
+	L40:
+		/*
+		*	 compute the householder transformation to reduce the
+		*	 j-th column of a to a multiple of the j-th unit vector.
+		*/
+		jj = j + m*j;
+		ajnorm = enorm(m - j, &a[jj]);
+		if (ajnorm == zero)
+			goto L100;
+		if (a[jj] < zero)
+			ajnorm = -ajnorm;
+		ij = jj;
+		for (i = j; i<m; i++)
+		{
+			a[ij] /= ajnorm;
+			ij += 1; /* [i+m*j] */
+		}
+		a[jj] += one;
+		/*
+		*	 apply the transformation to the remaining columns
+		*	 and update the norms.
+		*/
+		jp1 = j + 1;
+		if (jp1 < n)
+		{
+			for (k = jp1; k<n; k++)
+			{
+				sum = zero;
+				ij = j + m*k;
+				jj = j + m*j;
+				for (i = j; i<m; i++)
+				{
+					sum += a[jj] * a[ij];
+					ij += 1; /* [i+m*k] */
+					jj += 1; /* [i+m*j] */
+				}
+				temp = sum / a[j + m*j];
+				ij = j + m*k;
+				jj = j + m*j;
+				for (i = j; i<m; i++)
+				{
+					a[ij] -= temp*a[jj];
+					ij += 1; /* [i+m*k] */
+					jj += 1; /* [i+m*j] */
+				}
+				if ((pivot != 0) && (rdiag[k] != zero))
+				{
+					temp = a[j + m*k] / rdiag[k];
+					temp = dmax1(zero, one - temp*temp);
+					rdiag[k] *= sqrt(temp);
+					temp = rdiag[k] / wa[k];
+					if ((p05*temp*temp) <= MACHEP)
+					{
+						rdiag[k] = enorm(m - j - 1, &a[jp1 + m*k]);
+						wa[k] = rdiag[k];
+					}
+				}
+			}
+		}
+
+	L100:
 		rdiag[j] = -ajnorm;
 	}
 	/*
